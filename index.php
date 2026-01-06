e3b39f0f90341e92e8ddb742299070a6f2f8706e
Abi-kingyt <ashokkumarabishek2@gmail.com>
2026-01-06 16:26:44 +0100

    Factorisation PDO

--- index.php
+++ index.php
@@ -13,10 +13,8 @@
  * - Le mode d'erreur : le mode exception permet à PDO de nous prévenir violament quand on fait une erreur ;-)
  * - Le mode d'exploitation : FETCH_ASSOC veut dire qu'on exploitera les données sous la forme de tableaux associatifs
  */
-$pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-]);
+require('libraries/database.php');
+$pdo=getPdo();
 
 /**
  * 2. Récupération des articles
@@ -32,6 +30,6 @@ $articles = $resultats->fetchAll();
 $pageTitle = "Accueil";
 ob_start();
 require('templates/articles/index.html.php');
-$pageContent = ob_get_clean();
+$pageContent = ob_get_clean(); 
 
 require('templates/layout.html.php');
